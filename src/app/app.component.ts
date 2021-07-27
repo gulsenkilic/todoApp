0eccecc78af9a1414368ca83793174ae0c373647
gulsenkilic <glsnklc27@gmail.com>
2021-07-27 18:49:22 +0300

    last

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Model3, ToDo } from './model3';
+
 
 @Component({
   selector: 'app-root', //app comp. ini neerede kullanmak istiyorsun, app comp, ini çağırmak için gerekli etiket
@@ -6,11 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'] //app root çağırıldığında index.html sayfasında bu iki oge çalışacak
 })
 export class AppComponent {
-  user='Cinar';
-  items= [
-    {decription: "Kahvaltı", action:"No"},
-    {decription: "Sinema", action:"No"},
-    {decription: "Ders", action:"No"},
-    {decription: "Spor", action:"No"}
-  ];
+model3 = new Model3();
+isDisplay =false;
+
+getName(){
+  return this.model3.user;
+}
+getItems(){
+  if(this.isDisplay){
+    return this.model3.items;
+  }
+  return this.model3.items.filter((item: { action: any; }) => !item.action);
+}
+addItem(value: any){
+if(value!=" "){
+this.model3.items.push(new ToDo(value,false) );
+}
+}
+
 }
